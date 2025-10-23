1ed7580247359a05ce11c5f6e3ac10ba3c93c278
harshgupta9129 <rockstar221405@gmail.com>
2025-10-23 23:20:38 +0530

    Basics Started

---
+++ 01 Chapter/02_modules.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+print("Printing Jokes...")
+
+# This will print random jokes
+joke = pyjokes.get_joke()
+print(joke)
+
+"""Multi Lines Comment
+SO Thanks
+that was my program"""
---
+++ 01 Practice Set/02_Problem.py
@@ -0,0 +1,27 @@
+# C:\Users\harsh\OneDrive\Desktop\Python>python
+# Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# Ctrl click to launch VS Code Native REPL
+# >>> 5*1
+# 5
+# >>> 5*2
+# 10
+# >>> 5*3
+# 15
+# >>> 5*4
+# 20
+# >>> 5*4
+# 20
+# >>> 5*5
+# 25
+# >>> 5*6
+# 30
+# >>> 5*7
+# 35
+# >>> 5*8
+# 40
+# >>> 5*9
+# 45
+# >>> 5*10
+# 50
+# >>>
